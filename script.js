27bd04051fb1f69d6cb3d55d57f0299c119c526c
sneg333 <igortot@yandex.ru>
2021-11-17 13:48:06 +0300

    1 urok

--- script.js
+++ script.js
@@ -1,8 +1,29 @@
-"use strict";
+// "use strict";
 
-const men = {
-    name: "hot",
-    age: 26,
+// const men = {
+//     name: "hot",
+//     age: 26,
+// };
+
+// console.log(men.name);
+
+const moovieKveshtion = +prompt('сколько фильмов просмотрено ?', '');
+
+const moovieDB = {
+    count: moovieKveshtion,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
 };
 
-console.log(men.name);
+const 
+    a = prompt('Один из последних просмотренных фильмов', ''),
+    b = prompt('на сколько его оцените', ''),
+    c = prompt('Один из последних просмотренных фильмов', ''),
+    d = prompt('на сколько его оцените', '');
+
+    moovieDB.movies[a] = b;
+    moovieDB.movies[c] = d;
+
+    console.log(moovieDB);
