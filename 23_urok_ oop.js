82ce7569ed59126ea1eb473ea6e58a979557d6cc
sneg333 <igortot@yandex.ru>
2021-11-22 16:20:51 +0300

    23_urok_prototipi

---
+++ 23_urok_ oop.js
@@ -0,0 +1,33 @@
+// прототипно ориентированное программирование
+"use_scrict";
+
+const soldat = {
+    energi: 400,
+    rost: 180,
+    sayHello: function() {
+        console.log('hello');
+    }
+};
+
+const jon = {
+    energi: 100
+};
+// const jon = Object.create(soldat);
+// jon.sayHello();
+Object.setPrototypeOf(jon, soldat);
+
+jon.sayHello();
+console.log(jon);
+///////////////////////
+const soldat = {
+    energi: 400,
+    rost: 180,
+    sayHello: function() {
+        console.log('hello');
+    }
+};
+
+const jon = Object.create(soldat);
+jon.sayHello();
+
+
