14f8a86895deecb01c7b979ed1d33b8734fff6b2
sneg333 <igortot@yandex.ru>
2021-12-01 15:31:09 +0300

    urok_33_ forma_1

---
+++ 33_urok.js
@@ -0,0 +1,70 @@
+"use_strict";
+
+
+document.addEventListener('DOMContentLoaded', () => {
+
+    const movieDB = {
+        movies: [
+            "logan",
+            "liga spravedlivosti",
+            "la-la lend"
+        ]
+    };
+    
+    const addForm = document.querySelector('form.add'),
+    movieList = document.querySelector('.promo_interactiv_list'),
+    addInput = addForm.querySelector('.adding_input'),
+    checkbox = addForm.querySelectorAll('[type="checkbox"]');
+
+    addForm.addEventListener('submit', (event) => {
+        event.preventDefault();
+
+        let newFilm = addInput.value;
+        const favorit = checkbox.checked;
+
+        if (newFilm) {
+            //если название фильма большее 11 символов, то оно обрезается и ставится
+            if (newFilm.length > 11) {
+                newFilm = `${newFilm.substring(0, 12)}...`;
+            }
+
+            movieDB.movies.push(newFilm);
+            sortArr(movieDB.movies);
+            createMovieList(movieDB.movies, movieList);
+        }
+
+
+        event.target.reset();
+    });
+
+        const sortArr = (arr) => {
+            arr.sort();
+        }
+
+
+    function createMovieList(film, parent) {
+        parent.innerHTML = "";
+    
+        film.forEach((film, i) => {
+            parent.innerHTML += `
+                <li class="promo_inter_item">${i + 1} ${film}
+                    <div class="delete"></div>
+                    </li>
+            `;
+        });
+    }
+
+
+    sortArr(movieDB.movies);
+});
+
+
+
+
+
+//1 получаем элементы из html с которыми будем работать
+const fff = document.querySelector('.kakojto_class')
+
+
+
+
